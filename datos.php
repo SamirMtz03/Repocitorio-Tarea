90b026e3b35ff8d9a110c2386fbe879dc53d1110
SamirMtz03 <samir.martinez22@utim.edu.mx>
2023-06-26 13:25:30 -0600

    Actualizacion del GET
    Se actualizo el archivo datos.php ya que no se habia hecho conforme a los requerimientos

--- datos.php
+++ datos.php
@@ -5,5 +5,12 @@ $telefono = $_POST ['telefono'];
 $sitioweb = $_POST ['sitio_web'];
 $asunto = $_POST ['asunto'];
 $mensaje = $_POST ['mensaje'];
+
+/*$nombre = $_GET ['nombre'];
+$email = $_GET ['email'];
+$telefono = $_GET ['telefono'];
+$sitioweb = $_GET ['sitio_web'];
+$asunto = $_GET ['asunto'];
+$mensaje = $_GET ['mensaje'];*/
 echo $nombre. "/" . $email. "/" . $telefono. "/" . $sitioweb. "/" . $asunto. "/" . $mensaje;
 ?>
